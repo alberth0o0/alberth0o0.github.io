5116d87e5400022f9436e7af0a91ee41183be83a
Albert <pxsivs@gmail.com>
2026-03-15 05:08:39 -0400

    First (bad) ending !!!
    (game.js)
    If you decide to stay home after staying up all night, you will accidentally oversleep and miss your concert!
    
    (index.html)
    changed game title name from premade title to my game title ("Dazey Days")

--- projects/game/game.js
+++ projects/game/game.js
@@ -12,45 +12,61 @@ let knowledge = 0;
 //Make one function for each location
 function room() {
     clear();
-    print("\nBEEP!");
+    print("BEEP!");
     setTimeout(function() {
         print(" BEEP!");
         setTimeout(function() {
-            print(" BEEP!");
+            print("  BEEP!");
             setTimeout(function() {
-                },1000);
-        },500);
-    },500);
-    print("\nYou reluctantly throw your arms up to let your hand get in reach for your alarm clock. 12:00AM, August 15th on the clock.");
-    print("\nYou slowly raise your head and rubbed your eyes, then looked at the blue-ish moonlit room around you.");
-    print("\nYawwwwwwwn! After analyzing your surroundings, you look down to an absolute mess of stage plans, dance routines, and music sheets scattered all over the desk.");
-    print("\n\"Oh shoot! It's my concert today!\" You exclaimed to yourself. \"I must've passed out working on these.. My gosh..\"");
-    print("\nWith some coffee still left over in your cup by the top left corner of your desk, you continue working throughout the night.");
+                print("You reluctantly throw your arms up to let your hand get in reach for your alarm clock. 12:00AM, August 15th.");
+                print("You slowly raise your head and rubbed your eyes, then looked at the blue-ish moonlit room around you.");
+                print("Yawwwwwwwn! After analyzing your surroundings, you look down to an absolute mess of stage plans, dance routines, and music sheets scattered all over the desk.");
+                print("\"Oh shoot! It's my concert today!\" You exclaimed to yourself. \"I must've passed out working on these.. My gosh..\"");
+                print("With some coffee still left over in your cup by the top left corner of your desk, you continue working throughout the night.");
     
-    print("\n\nPress `enter` to continue");
+                print("\n\nAfter a while of planning, the sun's rays pierces through your window illuminating everything into visibility.");
+                print("You sit up from your chair and take a looooong stretch.");
+                print("4PM August 15th. Time flies by..");
+                print("You feel a bit sleepy but know the show must go on..");
+                print("Should you take a (walk) or (stay home)?");
+                },1000);
+        },750);
+    },750);
+    
     function processInput(input){
-        if (input.toLowerCase() === "") {
-            print("\n\nAfter a while of planning, the sun's rays pierces through your window illuminating everything into visibility.");
-            print("\nYou sit up from your chair and take a looooong stretch.");
-            print("\n4PM August 15th. Time flies by..");
-            print("\nYou feel a bit sleepy but know the show must go on..");
-            print("\nShould you take a (walk) or (stay home)?");
+        if (input.toLowerCase() === "walk") {
+            street();
         }
-        function processInput2(input){
-            if input
+
+        else if (input.toLowerCase() === "stay home") {
+            clear();
+            print("You decide to stay home and take a little nap as a reward for your hard work (you did stay up all night after all).");
+            setTimeout(function() {
+                print("...");
+                setTimeout(function() {
+                    print("...");
+                    setTimeout(function() {
+                        print("\nYou wake up and realize you have a concert to perform in a few hours. You quickly get ready and rush out the door, but unfortunately, you missed your show.");
+                        print("Better luck next time!");
+                        gameActive = false;
+                    },2000);
+                },2000);
+            },5000);
+        }
         else {
             stayHere();
-            waitThenCall(locationA);
+            waitThenCall(room);
         }
+        
     }
     waitForInput(processInput);
 }
 
-function locationB() {
+function street() {
     clear();
-    print("\nYou are in location B!");
-    print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationA");
+    print("You decide to take a walk to clear your head and get some fresh air. As you step outside, you feel the cool breeze on your face and the warmth of the sun on your skin. You take a deep breath and enjoy the moment.");
+    print("The sun makes the day nice, but it's rays are piercing your skin a bit too much.");
+    print("Amidst the heat, you see a ice cream shop across the street.\nDo you want to go get some (ice cream) or (keep walking)?");
     
     function processInput(input){
         if (input.toLowerCase() === "locationa") {
@@ -67,7 +83,7 @@ function locationB() {
 //very start. For this simple example, any input will bring you
 //to locationA
 function start(){
-    print("Dazey Daze\n\n");
+    print("Press `enter` to start...");
 
     function processInput(input){
             room();
