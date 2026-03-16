815a92ab9ea630b3e762d9b0f2f15c5c9374b2ea
Albert <pxsivs@gmail.com>
2026-03-16 17:50:39 -0400

    Good ending!
    Made some typo changes. A major implementation is the final ending for the game after the player chooses to go to the train station instead of the taxi!

--- projects/game/game.js
+++ projects/game/game.js
@@ -121,7 +121,7 @@ function street() {
                 clear();
                 print("You quickly call 911 to report the accident. The operator asks for your location and details about the incident.");
                 print("As you explain the situation, steel beams being carried by a crane above you SNAPS and falls on top of you.");
-                loop =+ 1;
+                loop += 1;
                 print("Press 'enter' to continue...");
             
                 gameActive = false;
@@ -163,22 +163,76 @@ function iceCreamShop() {
 
 function crosswalk() {
     clear();
-    print("You decide to chase the kitten and follow it across the street. As you run after it, you see a truck speeding towards the kitten! You quickly jump out of the way just in time, but unfortunately, you sprain your ankle in the process.");
-    print("The truck swerves back and forth. CRASH! The car hits a nearby light pole. Your heart races, trying to find any way out of this situation now with your ankle sprained.");
-    print("You feel helpless. You turn to look to the truck and suddenly metal cylinders start falling out of the truck towards you. You close your eyes and braced.");
-    knowledge += 1;
-    print("Press 'enter' to continue...");
+    if(loop === 0) {
+        print("You decide to chase the kitten and follow it across the street. As you run after it, you see a truck speeding towards the kitten! You quickly jump out of the way just in time, but unfortunately, you sprain your ankle in the process.");
+        print("The truck swerves back and forth. CRASH! The car hits a nearby light pole. Your heart races, trying to find any way out of this situation now with your ankle sprained.");
+        print("You feel helpless. You turn to look to the truck and suddenly metal cylinders start falling out of the truck towards you. You close your eyes and braced.");
+        loop += 1;
+        print("Press 'enter' to continue...");
+    }
+    else if (loop === 1){
+        print("You catch up with the cat and caught it in your arms. You look around and see the truck smoking and people gathering around it in the distance.");
+        print("You want to go away from the situation as fast as possible.");
+        print("Do you call a (taxi) or the (train)?");
+    }
 
     function processInput(input){
-        if (input.toLowerCase() === "") {
-            room();
+        if (loop === 0) {
+            if (input.toLowerCase() === "") {
+                room();
+            }
+            else {
+                stayHere();
+                waitThenCall(crosswalk());
+            }
         }
-        else {
-            stayHere();
-            waitThenCall(crosswalk());
+        else if (loop === 1) {
+            if (input.toLowerCase() === "taxi") {
+                clear();
+                print("You called a taxi to take you away from the scene. You get in and give the driver your concert's location. As you drive away, you stroke the kitten's head and try to calm it down.");
+                print("Staring out the window, you slowly close your eyes to get some rest from this traumatic experience.");
+                setTimeout(function() {
+                    print("...");
+                    setTimeout(function() {
+                        print("...");
+                        setTimeout(function() {
+                            print("\nWEE-WOO-WEE-WOO! You wake up to the sound of sirens");
+                            print("You look out the window to see the situation you just left.");
+                            print("Suddenly, BOOM! The truck explodes into flames, engulfing the sky with smoke and fire. You watched in horror.");
+                            print("The taxi driver, startled, swerves into incoming traffic and crashes.");
+                            gameActive = false;
+                        },2000);
+                    },2000);
+                },2000);
+            }
+            else if (input.toLowerCase() === "train") {
+                trainStation();
+            }
+            else {
+                stayHere();
+                waitThenCall(crosswalk());
+            }
         }
+
     }
     waitForInput(processInput);
+}
+
+function trainStation() {
+    clear();
+    setTimeout(function() {
+        print("You catch your breath by the stairs leading down to the train station.");
+        print("After, the train comes just in time for you to hop on, taking you to your concert. The cat, still in your arms, purrs as you stroke its head.");
+    },20000);
+    concert();
+}
+
+function concert() {
+    clear();
+    print("\nYou arrive at your concert and get ready backstage. The cat sits on your dressing table as you pass out the plans you worked so hard on to your crew. You take a deep breath and prepare for the show.");
+    print("The curtains open and the crowd goes wild. You perform your heart out, giving the best show of your life. The cat watches from the side of the stage, purring in approval.\n\"What a day...:\" You say to yourself as you take your final bow.");
+    gameActive = false;
+    //Good ending!
 }
 
 function start(){
