e41a27c99b799515776e8fec6641d6dd6f2b6b08
Albert <pxsivs@gmail.com>
2026-03-16 18:51:51 -0400

    First room completed
    Gumball starts off in his room and checks for his pages in his desk and closet

--- projects/game/README.md
+++ projects/game/README.md
@@ -1,4 +1,4 @@
-# The Missing Homework
+# The Final Pages
 
 ## Setting
 The setting starts at Gumball's house. The player navigates throughout his house to find pages and interact with his family and friends to find his essay pages. He can also enter school for the pages before he MUST go to Ms. Simian's classroom and turn in his work (completed or not).
@@ -22,4 +22,7 @@ graph TD;
 Ms. Simian assigned a 3 page essay that Gumball didn't want to do so he convinced his sister to write it for him. On the day the paper was due, Gumball finds out that it's missing from his backpack! He must find all of the pages before the period creeps up on him.
 
 ## Global Variables
-The only global variable would be `pages` counting the amount he finds. The amount of pages would determine the ending Gumball would be in!
+The global variable would be:
+`pages` counting the amount he finds. The amount of pages would determine the ending Gumball would be in!
+`moves` basically the time he has before his class with Ms. Simian to turn in his essay pages.
+
--- projects/game/game.js
+++ projects/game/game.js
@@ -2,32 +2,78 @@ let gameActive = true; //this variable is required.
                        //to stop the game, set it to false.
 
 //Declare your other global variables here
-
+let pages = 0;
 
 //If you need, add any "helper" functions here
 
-
 //Make one function for each location
-function locationA() {
+function room() {
     clear();
-    print("\nYou are in location A!");
-    print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationB");
+    print("\n\"WAKE UP!\" Anais wakes Gumball up and tells him to get ready for school. Gumball is still sleepy and doesn't want to get up.");
+    print("\n\"5 more minutes!\" Gumball says. Anais rolls her eyes and tells him to get up now and get his paper ready to turn in.");
+    print("\n\"Fine! It's in my backpack!\" Gumball says. He gets up and goes to his backpack to find his paper is not there.");
+    print("\n\"Where is it?\" Gumball says. \"I know I put it in there!\" He starts to panic and looks around the room for it.");
+    print("\nWhere do you want to look for the paper? Say one of these choices:" +
+        "\n\tdesk" +
+        "\n\tcloset" +
+        "\n\tliving room");
     
     function processInput(input){
-        if (input.toLowerCase() === "locationb") {
-            locationB();
+        if (input.toLowerCase() === "desk") {
+            desk();
         } else {
             stayHere();
-            waitThenCall(locationA);
+            waitThenCall(room);
         }
     }
     waitForInput(processInput);
 }
 
-function locationB() {
+function desk() {
     clear();
-    print("\nYou are in location B!");
+    moved --;
+    print("\nGumball looks in his desk to find... nothing! \"WHAT THE WHAT?!\" He is very confused and starts to panic more.");
+    print("\nWhere do you want to look for the paper? Say one of these choices:" +
+        "\n\tcloset" +
+        "\n\tliving room");
+    function processInput(input){
+        if (input.toLowerCase() === "closet") {
+            closet();
+        } else if (input.toLowerCase() === "living room") {
+            livingRoom();
+        } else {
+            stayHere();
+            waitThenCall(desk);
+        }
+    }
+    waitForInput(processInput);
+}
+
+function closet() {
+    clear();
+    moved --;
+    print("\nGumball looks in the closet to find... nothing! \"Bro.. This isn't funny.\" He is very confused and starts to panic more.");
+    print("\nWhere do you want to look for the paper? Say one of these choices:" +
+        "\n\tdesk" +
+        "\n\tliving room");
+    function processInput(input){
+        if (input.toLowerCase() === "desk") {
+            desk();
+        } else if (input.toLowerCase() === "living room") {
+            livingRoom();
+        } else {
+            stayHere();
+            waitThenCall(closet);
+        }
+    }
+    waitForInput(processInput); 
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+function livingRoom() {
+    clear();
+    print("\nGumball slides down the stairs and goes to the living room.");
     print("\nWhere do you want to go next? Say one of these choices:" +
         "\n\tlocationA");
     
@@ -49,7 +95,7 @@ function start(){
     print("Welcome to my game! Press any key to start");
 
     function processInput(input){
-            locationA();
+            room();
     }
     waitForInput(processInput);
 }
